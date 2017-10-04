9a71bf82aa1cd91a12f1a0073f7bc99f843a1a89
xutao <xutao@fruitday.com>
2017-10-04 17:37:14 +0800

    commit 卖票程序

---
+++ src/main/java/basic/deadlock/DeadlockDemo.java
@@ -0,0 +1,42 @@
+package basic.deadlock;
+
+
+class LockA{}
+class LockB{}
+
+//常见情景之一，同步的嵌套
+class Task1 implements  Runnable{
+    public void run(){
+        synchronized (LockA.class){
+            try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+            synchronized (LockB.class){
+                System.out.println("Task1 output");
+            }
+        }
+    }
+}
+
+class Task2 implements  Runnable{
+    public void run(){
+        synchronized (LockB.class){
+            try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+            synchronized (LockA.class){
+                System.out.println("Task2 output");
+            }
+        }
+    }
+}
+
+
+public class DeadlockDemo {
+    public static void main(String[]args){
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+
+        t1.start();
+        t2.start();
+    }
+}
